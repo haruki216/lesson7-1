f8fdffa046de605cac06c9356f19a06520dda445
EC2 Default User <ec2-user@ip-172-31-3-28.ec2.internal>
2024-05-07 17:24:05 +0900

    lesson8-4

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body class="antiaiased">
+       <form action="/posts" method="POST">
+           @csrf
+           <div class="title">
+               <h2>title</h2>
+               <input type="text" name=post[title] placeholder="タイトル">
+            <div class="body">
+                <h2>body</h2>
+                <textarea name="post[body]" placeholder="今日も一日お疲れ様でした"></textarea>
+             
+            </div>   
+               <input type="submit"value ="store">   
+           </div>
+           
+       </form>
+        <div class="footer">
+        <a href="/">戻る</a>
+        </div>        
+        
+    </body>
+</html>
+Copy
